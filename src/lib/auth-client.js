b60db58f2532af2e1c1abca634769aa04ee9e176
SUMON <sumon01333581557.gmail.com>
2026-04-27 14:13:53 +0600

    first commit

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: "https://news-baliadangi.vercel.app"
+    baseURL: "http://localhost:3000"
 })
 
 export const { signIn, signUp, useSession } = createAuthClient()
